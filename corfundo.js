0805b4750170a837c2d0b740938d6d3923e61cb9
Tiago C. dos Santos <caceraghi@gmail.com>
2024-05-17 10:47:22 -0300

    versao 0.0.4 cor fundo

--- corfundo.js
+++ corfundo.js
@@ -2,7 +2,7 @@ const conteudo = document.querySelector('.conteudo');
 const color = document.querySelector('input');
 
 const corPadrao = '#fff';
-conteudo.style.backgroundColor = `$codPadrao`;
+conteudo.style.backgroundColor = `${codPadrao}`;
 
 function mudarCorFundo(){
     conteudo.style.backgroundColor = `${color.value}`;
